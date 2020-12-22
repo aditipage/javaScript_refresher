0522fa7feee88c2ce924f067bf3d741dc4fae73a
Aditi Page <aditipage@gmail.com>
2020-12-22 17:15:14 -0500

    Basic syntax

---
+++ play.js
@@ -0,0 +1,18 @@
+var name = 'aditi';
+console.log(name);
+
+var n = 10;
+console.log(n);
+
+var flag = true;
+console.log(flag);
+
+function isGreater(num){
+    if (num >= 10){
+        return true;
+    }
+    return false;
+}
+
+var b = isGreater(n);
+console.log(b);
